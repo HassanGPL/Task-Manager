d655eaff18683e8b9aa2c407d637b61a51078b3f
Hassan Ahmed <ahmedhassanmana@gmail.com>
2023-09-16 23:10:58 +0300

    Add Show All Tasks functionality

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,12 +2,12 @@ const Task = require('../models/task');
 
 
 exports.getAllTasks = async (req, res, next) => {
-    // try {
-    //     const tasks = await Task.find({});
-    //     res.status(200).json({ tasks });
-    // } catch (error) {
-    //     res.status(500).json({ msg: error });
-    // }
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({ tasks });
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
 }
 
 exports.getSingleTask = async (req, res, next) => {
