7899ccbbe3fdeaf4ebb0925fbe7a30e53b3b6044
Hassan Ahmed <ahmedhassanmana@gmail.com>
2023-09-16 23:12:26 +0300

    Add Update Task functionality

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -24,19 +24,19 @@ exports.getSingleTask = async (req, res, next) => {
 }
 
 exports.patchUpdateTask = async (req, res, next) => {
-    // try {
-    //     const { id: taskID } = req.params;
-    //     const task = await Task.findByIdAndUpdate({ _id: taskID }, req.body, {
-    //         new: true,
-    //         runValidators: true
-    //     });
-    //     if (!task) {
-    //         return res.status(404).json({ msg: `No Task With This ID : ${taskID}` });
-    //     }
-    //     res.status(200).json({ task });
-    // } catch (error) {
-    //     res.status(500).json({ msg: error });
-    // }
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findByIdAndUpdate({ _id: taskID }, req.body, {
+            new: true,
+            runValidators: true
+        });
+        if (!task) {
+            return res.status(404).json({ msg: `No Task With This ID : ${taskID}` });
+        }
+        res.status(200).json({ task });
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
 }
 
 exports.deleteTask = async (req, res, next) => {
